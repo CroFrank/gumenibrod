f810aaee3ab6ac73b8330dca0f33f5f8e4bf05be
Fran <fducekuce@gmail.com>
2025-07-11 21:17:52 +0200

    add sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://gumenibrod.hr",
+  generateRobotsTxt: true, // generira robots.txt
+  sitemapSize: 5000,
+  changefreq: "weekly",
+  priority: 0.7,
+}
